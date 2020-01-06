cc900559b29ef3a83954a45e87fbebd32b81b397
Kay <Shmfih@gmail.com>
2020-01-06 13:10:15 +0700

    Updates

---
+++ precache-manifest.ded80840e6d8118e6ec4a4d3e4b4824a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78bf5962f5ab3ad8332434b3a39a26e4",
+    "url": "/llab-orders/index.html"
+  },
+  {
+    "revision": "898fed493ce79dfc777e",
+    "url": "/llab-orders/static/css/main.95b7148b.chunk.css"
+  },
+  {
+    "revision": "3468f9fe689af8433886",
+    "url": "/llab-orders/static/js/2.310ab588.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/llab-orders/static/js/2.310ab588.chunk.js.LICENSE"
+  },
+  {
+    "revision": "898fed493ce79dfc777e",
+    "url": "/llab-orders/static/js/main.dd34cf8e.chunk.js"
+  },
+  {
+    "revision": "6d58477c260c993512a3",
+    "url": "/llab-orders/static/js/runtime-main.373b2249.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/llab-orders/static/media/logo.5d5d9eef.svg"
+  }
+]);
